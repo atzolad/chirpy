23c5fcb5c60fd78f0235dfb591309834c40ec8e2
atzolad <atzolad@gmail.com>
2025-10-03 21:35:34 -0700

    Added fileserver

--- main.go
+++ main.go
@@ -6,13 +6,19 @@ import (
 )
 
 func main() {
+
+	const filepathRoot = "."
+	const port = "8080"
+
 	mux := http.NewServeMux()
+	mux.Handle("/", http.FileServer(http.Dir(filepathRoot)))
 
 	newServer := http.Server{
-		Addr:    ":8080",
+		Addr:    ":" + port,
 		Handler: mux,
 	}
 
+	log.Printf("Serving files from %s on port: %s\n", filepathRoot, port)
 	if err := newServer.ListenAndServe(); err != nil {
 		log.Fatal(err)
 	}
