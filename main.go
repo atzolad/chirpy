0702248c24e39473f45d8f14974f2f4bf10b5de1
atzolad <atzolad@gmail.com>
2025-10-14 22:16:00 -0400

    Added readiness endpoint

--- main.go
+++ main.go
@@ -5,21 +5,33 @@ import (
 	"net/http"
 )
 
+// func readinessCheck(w http.ResponseWriter, r *http.Request) {
+// 	w.Write([]byte("Content-Type: text/plain; charset=utf-8"))
+// }
+
 func main() {
 
 	const filepathRoot = "."
 	const port = "8080"
 
 	mux := http.NewServeMux()
-	mux.Handle("/", http.FileServer(http.Dir(filepathRoot)))
+	mux.Handle("/app/", http.StripPrefix("/app/", http.FileServer(http.Dir(filepathRoot))))
 
 	newServer := http.Server{
 		Addr:    ":" + port,
 		Handler: mux,
 	}
 
+	mux.HandleFunc("/healthz", func(w http.ResponseWriter, req *http.Request) {
+
+		w.Header().Set("Content-Type:", "text/plain; charset=utf-8")
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("OK"))
+	})
+
 	log.Printf("Serving files from %s on port: %s\n", filepathRoot, port)
 	if err := newServer.ListenAndServe(); err != nil {
 		log.Fatal(err)
 	}
+
 }
